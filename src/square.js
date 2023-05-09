f21ebeca889ea2aa7a6ef278325f596ae3d06be9
AlikoGrace <gracealiko09@gmail.com>
2023-05-09 22:42:34 +0000

    a game that makes use of all react basics

---
+++ src/square.js
@@ -0,0 +1,17 @@
+import {useState} from "react";
+
+function Square({value,onSquareClick}){
+
+
+    return(
+        <button
+            onClick={onSquareClick}
+                className='square'
+        >
+            {value}
+        </button>
+        )
+
+}
+
+export default Square
